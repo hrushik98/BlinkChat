9d8507223d33e152cda11e0d107203733297457f
Sharan Irani <iranisharan@gmail.com>
2024-11-15 08:55:44 +0530

    Pakka Final Commit

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://blinkchat-wvq6.onrender.com/login", {
+			const socket = io("https://blinkchat-wvq6.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
