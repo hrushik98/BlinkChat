a9c1eb904393f5b141d759503fab9ccec98cc2c3
Sharan Irani <iranisharan@gmail.com>
2024-11-15 08:23:40 +0530

    Deployment

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,13 +1,11 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
-/*
 import { getReceiverSocketId, io } from "../socket/socket.js";
-*/
 
 export const sendMessage = async (req, res) => {
 	try {
 		const { message } = req.body;
-		const { id:receiverId } = req.params;
+		const { id: receiverId } = req.params;
 		const senderId = req.user._id;
 
 		let conversation = await Conversation.findOne({
@@ -29,20 +27,18 @@ export const sendMessage = async (req, res) => {
 		if (newMessage) {
 			conversation.messages.push(newMessage._id);
 		}
-        
-		
-        
-		
+
+		// await conversation.save();
+		// await newMessage.save();
+
+		// this will run in parallel
 		await Promise.all([conversation.save(), newMessage.save()]);
-        
-        /*
+
 		// SOCKET IO FUNCTIONALITY WILL GO HERE
 		const receiverSocketId = getReceiverSocketId(receiverId);
 		if (receiverSocketId) {
-			// io.to(<socket_id>).emit() used to send events to specific client
 			io.to(receiverSocketId).emit("newMessage", newMessage);
 		}
-            */
 
 		res.status(201).json(newMessage);
 	} catch (error) {
@@ -58,7 +54,7 @@ export const getMessages = async (req, res) => {
 
 		const conversation = await Conversation.findOne({
 			participants: { $all: [senderId, userToChatId] },
-		}).populate("messages"); /// ACTUAL MESSAGESs
+		}).populate("messages"); // NOT REFERENCE BUT ACTUAL MESSAGES
 
 		if (!conversation) return res.status(200).json([]);
 
